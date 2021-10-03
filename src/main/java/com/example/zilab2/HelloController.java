d67130594c2d31eb3cc44f72561850224ab07dd1
a.stepanova <ps.ovsyanka@gmail.com>
2021-10-03 12:21:22 +0500

    ппппппппппппппп

--- src/main/java/com/example/zilab2/HelloController.java
+++ src/main/java/com/example/zilab2/HelloController.java
@@ -2,41 +2,120 @@ package com.example.zilab2;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label labelE;
+    @FXML
+    private Label labelN;
+    @FXML
+    private Label labelD;
+    @FXML
+    private TextField inputText;
+    @FXML
+    private TextField outputText;
+
+    BigInteger p, q, e, n, m, d;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onInCodeButtonClick() throws IOException {
 
-        Long p, q, n;
-        //welcomeText.setText(RandomPrime().toString());
+        p = RandomPrime();
+        q = RandomPrime();
+        n = p.multiply(q);
+        m = p.add(new BigInteger("-1")).multiply(q.add(new BigInteger("-1")));
+        e = Calculate_E(p, q);
+        //d = Euclid(p, q, e);
 
-        try(FileWriter writer = new FileWriter("PrimeList.txt", true))
-        {
-            for (BigInteger i = new BigInteger("100000000000"); i.compareTo(new BigInteger("200000000000")) == -1; i = i.add(new BigInteger("1"))){
-                String res = isPrime(i) ? i.toString() : "";
-                if (res != ""){
-                    System.out.println(res);
-                    writer.write(res);
-                    writer.append('\n');
-                }
-            }
+        //double k = (int)Math.log10(p*q)/Math.log10(2);
+        char[] text = inputText.getText().toCharArray();
+        char[] res = new char [text.length];
+        int j = 0;
+        for (int i =0;i < text.length; i ++){
+            //res[j] = text[i];
+            double c = (int) text[i];
+            //double v = Math.pow(c, e);
+            //res[i] = (char)(int)v;
         }
-        catch(IOException ex){
 
-            System.out.println(ex.getMessage());
-        }
+        //outputText.setText();
+        labelE.setText(String.valueOf(e));
+        labelN.setText(String.valueOf(n));
+        labelD.setText(String.valueOf(m));
+
+        //welcomeText.setText(line);
+
     }
 
+
+    protected void onDeCodeButtonClick() {
+        char[] text = inputText.getText().toCharArray();
+
+
+    }
+
+
+
+
+
+    double Euclid(double p, double q, double e){
+        double m = (p - 1) * (q - 1);
+        double d, y;
+        double r = e % m;
+
+        double[][] E = new double[][] {{0, 1}, {0, 1}};
+
+        while (r != 0){
+            E = MatrixMultiply(E, q);
+            e = m;
+            m = r;
+            r = e % m;
+        }
+
+        return e;
+    }
+
+    double[][] MatrixMultiply(double[][] E, double q){
+        double[][] M = new double[2][2];
+
+        M[0][0] = E[0][0] * 0 + E[0][1] * 1;
+        M[0][1] = E[0][0] * 1 + E[0][1] * q * -1;
+        M[1][0] = E[1][0] * 0 + E[1][1] * 1;
+        M[1][1] = E[1][0] * 1 + E[1][1] * q * -1;
+
+        return M;
+    }
+
+    BigInteger RandomPrime() throws IOException {
+        Random rand = new Random();
+        int lineNumber = rand.nextInt(2000);
+        return new BigInteger(Files.readAllLines(Paths.get("PrimeList.txt")).get(lineNumber));
+    }
+
+    BigInteger Calculate_E (BigInteger m, BigInteger n){
+        BigInteger e = n.add(new BigInteger("-1"));
+        while (e.gcd(m).equals(new BigInteger("1"))){
+            e.add(new BigInteger("-1"));
+        }
+        return  e;
+    }
+
+
+    public double gcd(double a, double b) {
+        if (b==0) return a;
+        return gcd(b,a%b);
+    }
 
     /*BigInteger RandomPrime(){
         Random rand = new Random();
