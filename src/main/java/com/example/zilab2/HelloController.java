d50c794a3f82576fd29ee08a74c22f67ce914b58
a.stepanova <ps.ovsyanka@gmail.com>
2021-10-08 09:31:00 +0500

    готовая штука

--- src/main/java/com/example/zilab2/HelloController.java
+++ src/main/java/com/example/zilab2/HelloController.java
@@ -1,13 +1,11 @@
 package com.example.zilab2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.nio.file.Files;
@@ -15,87 +13,116 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class HelloController {
-    @FXML
-    private Label labelE;
-    @FXML
-    private Label labelN;
-    @FXML
-    private Label labelD;
-    @FXML
-    private TextField inputText;
-    @FXML
-    private TextField outputText;
-
-    BigInteger p, q, e, n, m, d;
 
     @FXML
-    protected void onInCodeButtonClick() throws IOException {
+    private TextField p_inputText;
+    @FXML
+    private TextField q_inputText;
+    @FXML
+    private TextField d_Text;
+    @FXML
+    private TextField n_Text;
+    @FXML
+    private TextField m_Text;
+    @FXML
+    private TextField e_Text;
+    @FXML
+    private Button open_in_file_button;
+    @FXML
+    private Button open_out_file_button;
 
-        p = RandomPrime();
-        q = RandomPrime();
+    @FXML
+    protected void onEncryptButtonClick() throws IOException {
+        BigInteger p, q, e, n, m, d;
+
+        p = new BigInteger(p_inputText.getText());////100519  3583
+        q = new BigInteger(q_inputText.getText());////77017  2591
         n = p.multiply(q);
         m = p.add(new BigInteger("-1")).multiply(q.add(new BigInteger("-1")));
-        e = Calculate_E(p, q);
-        //d = Euclid(p, q, e);
+        e = Find_e(m);
+        d = Find_d(e, m);
 
-        //double k = (int)Math.log10(p*q)/Math.log10(2);
-        char[] text = inputText.getText().toCharArray();
-        char[] res = new char [text.length];
-        int j = 0;
-        for (int i =0;i < text.length; i ++){
-            //res[j] = text[i];
-            double c = (int) text[i];
-            //double v = Math.pow(c, e);
-            //res[i] = (char)(int)v;
+        String content;
+        ArrayList<Character> res = new ArrayList<>();
+
+        content = new String(Files.readAllBytes(Paths.get("in.txt")));
+
+        for (int i = 0; i < content.length(); i++)
+        {
+            BigInteger index = new BigInteger((Integer.toString(content.charAt(i))));
+            index = index.pow(e.intValue()).mod(n);
+
+            res.add((char)index.intValue());
         }
 
-        //outputText.setText();
-        labelE.setText(String.valueOf(e));
-        labelN.setText(String.valueOf(n));
-        labelD.setText(String.valueOf(m));
 
-        //welcomeText.setText(line);
-
-    }
-
-
-    protected void onDeCodeButtonClick() {
-        char[] text = inputText.getText().toCharArray();
-
-
-    }
-
-
-
-
-
-    double Euclid(double p, double q, double e){
-        double m = (p - 1) * (q - 1);
-        double d, y;
-        double r = e % m;
-
-        double[][] E = new double[][] {{0, 1}, {0, 1}};
-
-        while (r != 0){
-            E = MatrixMultiply(E, q);
-            e = m;
-            m = r;
-            r = e % m;
+        ClearFile("out.txt");
+        FileWriter fw = new FileWriter("out.txt", true);
+        for (Character s: res) {
+            fw.append(s);
         }
 
-        return e;
+        fw.flush();
+        d_Text.setText(d.toString());
+        n_Text.setText(n.toString());
+        e_Text.setText(e.toString());
+        m_Text.setText(m.toString());
+
     }
 
-    double[][] MatrixMultiply(double[][] E, double q){
-        double[][] M = new double[2][2];
+    @FXML
+    protected void onDecipherButtonClick() throws IOException{
+        BigInteger n, d;
+        d = new BigInteger(d_Text.getText());
+        n = new BigInteger(n_Text.getText());
 
-        M[0][0] = E[0][0] * 0 + E[0][1] * 1;
-        M[0][1] = E[0][0] * 1 + E[0][1] * q * -1;
-        M[1][0] = E[1][0] * 0 + E[1][1] * 1;
-        M[1][1] = E[1][0] * 1 + E[1][1] * q * -1;
+        String content;
+        ArrayList<Character> res = new ArrayList<>();
 
-        return M;
+        content = new String(Files.readAllBytes(Paths.get("out.txt")));
+
+        for (int i = 0; i < content.length(); i++)
+        {
+            BigInteger index = new BigInteger((Integer.toString(content.charAt(i))));
+            index = index.pow(d.intValue()).mod(n);
+
+            res.add((char)index.intValue());
+        }
+
+        ClearFile("in.txt");
+        FileWriter fw = new FileWriter("in.txt", true);
+        for (Character s: res) {
+            fw.append(s);
+        }
+
+        fw.flush();
     }
+
+    @FXML
+    protected void onGenerateButtonClick() throws IOException{
+        p_inputText.setText(RandomPrime().toString());
+        q_inputText.setText(RandomPrime().toString());
+    }
+
+    @FXML
+    protected void onOpenInFileButtonClick() throws IOException {
+        //File f = new File("in.txt");
+        ProcessBuilder pb = new ProcessBuilder("Notepad.exe", "in.txt");
+        pb.start();
+    }
+
+    @FXML
+    protected void onOpenOutFileButtonClick() throws IOException {
+        //File f = new File("in.txt");
+        ProcessBuilder pb = new ProcessBuilder("Notepad.exe", "Out.txt");
+        pb.start();
+    }
+
+    private void ClearFile (String fileName) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        fileWriter.close();
+    }
+
 
     BigInteger RandomPrime() throws IOException {
         Random rand = new Random();
@@ -103,38 +130,38 @@ public class HelloController {
         return new BigInteger(Files.readAllLines(Paths.get("PrimeList.txt")).get(lineNumber));
     }
 
-    BigInteger Calculate_E (BigInteger m, BigInteger n){
-        BigInteger e = n.add(new BigInteger("-1"));
-        while (e.gcd(m).equals(new BigInteger("1"))){
-            e.add(new BigInteger("-1"));
-        }
-        return  e;
-    }
 
+    private BigInteger Find_e(BigInteger m)
+    {
+        BigInteger e = m.add(new BigInteger("-1"));
 
-    public double gcd(double a, double b) {
-        if (b==0) return a;
-        return gcd(b,a%b);
-    }
+        /*while (!e.gcd(m).equals(new BigInteger("1"))) {
+            e = e.add(new BigInteger("-1"));
+        }*/
 
-    /*BigInteger RandomPrime(){
-        Random rand = new Random();
-        Long n = rand.nextLong() + 100000000000L;
-        if (n < 0)
-            n *= -1;
-        while(!isPrime(n)){
-            n += 1;
-        }
-        return BigInteger.valueOf(n);
-    }*/
-
-    public static boolean isPrime(BigInteger x){
-        for (BigInteger i = new BigInteger("2"); i.compareTo(x.sqrt().add(new BigInteger("1"))) == -1; i = i.add(new BigInteger("1"))){
-            if (x.mod(i) == BigInteger.ZERO) {
-                return false;
+        for (BigInteger i = new BigInteger("2"); i.compareTo(m.add(new BigInteger("1"))) != -1; i = i.add(new BigInteger("1")))
+            if ((m.mod(e).equals(BigInteger.ZERO)) && (e.mod(i).equals(BigInteger.ZERO))) //если имеют общие делители
+            {
+                e = e.add(new BigInteger("-1"));
+                i = new BigInteger("1");
             }
+
+        return e;
+    }
+
+    private BigInteger Find_d(BigInteger e, BigInteger m)
+    {
+        BigInteger d = new BigInteger("10");
+
+        while (true)
+        {
+            if (e.multiply(d).mod(m).equals(new BigInteger("1")))
+                break;
+            else
+                d = d.add(new BigInteger("1"));
         }
-        return true;
+
+        return d;
     }
 
 }
